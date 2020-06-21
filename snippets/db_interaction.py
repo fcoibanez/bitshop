6be40e4d55cc906769b28301561a2bd33831cf02
fcoibanez <francisco.ibanez@gmail.com>
2020-06-20 21:55:43 -0400

    db interaction snippets started

---
+++ snippets/db_interaction.py
@@ -0,0 +1,23 @@
+import bitshop
+from bitshop.data import rest
+from datetime import datetime
+import os
+import csv
+wd = os.getcwd()
+
+# Read the API Keys
+keys_dict = {}
+with open(os.getcwd() + '/snippets/keys.csv', newline='', encoding='ASCII') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        keys_dict[row[0]] = row[1]
+
+data = bitshop.DataFetch(api_keys=keys_dict)
+data.build_meta()
+data.meta
+data.candles(
+    sid='BTC-USD',
+    start_dt=datetime(2020, 5, 20),
+    end_dt=datetime(2020, 5, 23),
+    frequency=86400
+)
